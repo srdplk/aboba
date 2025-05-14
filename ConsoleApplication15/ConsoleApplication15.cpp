ea04e5c6bf1b94e8c0d52adb36bde67acb728bfb
Student-2 GameSchool <stud2.gsch124@outlook.com>
2025-05-14 19:23:32 +0300

    metanit введение в строк

--- ConsoleApplication15/ConsoleApplication15.cpp
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -8,13 +8,13 @@ using namespace std;
 // system("chph 1251");
 //setlocale(LC_ALL, "Russian";
 
-unsigned int you = 0;
+string you = 0;
 int pc = 0;
 unsigned int ro;
 unsigned int i;
+		string q[3] = { "ножницы", "камень", "бумага" };
 
 void age() {//
-	srand(time(NULL));
 
 	cout << "Введите количество раундов\n";
 
@@ -24,17 +24,18 @@ void age() {//
 		cout << "Введите ваше значение:\n ";
 		cin >> you;
 		
-		string q[3] = { "ножницы", "камень", "бумага" };
 		
 		pc = rand() % 3;
 		cout << "PC = " << pc << endl;
+		for (int i = 0; i < 3; i++) {
 
-		if (you >= 3) {
+		if (you != q[i]) {
 			cout << "Error" << endl;
 			return age();
 		}
+	}
 
-		if (you == 0 && pc == 2 || you == 2 && pc == 1 || you == 1 && pc == 0) {
+		if (you == "камень" && pc[r] == "ножницы" || you == 2 && pc == 1 || you == 1 && pc == 0) {
 			cout << " You win!" << endl;
 		}
 		else if (you == pc) {
@@ -47,10 +48,15 @@ void age() {//
 	}
 }
 	int main() {
+	srand(time(NULL));
 		setlocale(LC_ALL, "Russian");
-		cout << "Введите число, где 0 - ножницы " << endl << "1 - это камень " << endl << "2 это бумага" << endl;
-		age();
-		cout << "You: " << you << endl << "Pc: " << pc << endl;
+		//cout << "Введите число, где 0 - ножницы " << endl << "1 - это камень " << endl << "2 это бумага" << endl;
+		//age();
+		//cout << "You: " << you << endl << "Pc: " << pc << endl;
+		// 
+		int a = rand() % 3;
+
+		cout << q[a];
 		//string bot[3] = {"test","rom","uijjkjlljl;"};
 		//char h[5] = { 't','e','s','t', '!'};
 		//for (int i = 0; i < 3; i++) {
@@ -61,13 +67,3 @@ void age() {//
 	}
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
